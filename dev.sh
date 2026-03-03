04429faefc4d7863f15c77a96053d449c926c59b
Travis Brown-John <travis@bigidea.games>
2026-03-02 16:00:52 -0800

    Use dev builds for faster iteration (~1s incremental)

--- dev.sh
+++ dev.sh
@@ -13,7 +13,7 @@ VITE_PID=$!
 cd ..
 
 # Watch Rust source and rebuild on change
-cargo watch -w src -s "wasm-pack build --target web --out-dir www/pkg 2>&1 | tail -1 && echo '✅ WASM rebuilt'" &
+cargo watch -w src -s "wasm-pack build --dev --target web --out-dir www/pkg 2>&1 | tail -1 && echo '✅ WASM rebuilt'" &
 WATCH_PID=$!
 
 trap "kill $VITE_PID $WATCH_PID 2>/dev/null; exit" INT TERM
